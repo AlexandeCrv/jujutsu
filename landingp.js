d606431a6459873f7747ba9e94e472a937647204
AlexandeCrv <alexandecarvalho318@gmail.com>
2024-04-15 17:42:22 -0300

    modificado

--- landingp.js
+++ landingp.js
@@ -1,110 +1,78 @@
-var s1 = document.getElementById('s1')
-var s2 = document.getElementById('s2')
-var s3 = document.getElementById('s3')
-var s4 = document.getElementById('s4')
+let s2 = document.getElementById('s2')
 function entrar1(){
-    s1.innerHTML = 'O protagonista itadore se torna o portador do sukuna'
-    document.body.style.color = "#FFFFFF"
+    alert("O protagonista itadore se torna o portador do sukuna")
     
-    
-   
+    // s2.style.backgroundColor = "#fff"
+    // s2.style.width = '100px'
+    // s2.style.height = '100px'
+    // s2.style.marginLeft = '200px'
+    // s2.style.position = 'absolute'
+    // s2.style.borderRadius = '20px'
 }
 
-function sai1(){
-s1.innerHTML = ""
 
-}
 
 
 function entrar2(){
-    s1.innerHTML = 'Somos apresentados a escola jujutsu e a so diretor'
-    document.body.style.color = "#FFFFFF"
+   alert('Somos apresentados a escola jujutsu e a so diretor')
 }
-function sai2(){
-    s1.innerHTML = ""
-}
+
+
 
 
 function entrar3(){
-    s1.innerHTML = 'Conhecemos a Nobara e vamos a uma missao nivel especial.'
-    document.body.style.color = "#FFFFFF"
+  alert('Conhecemos a Nobara e vamos a uma missao nivel especial.')
     
-}
-function sai3(){
-    s1.innerHTML = ""
 }
 
 function entrar4(){
-    s1.innerHTML = 'O itadore permite que o sukuna saia e acabe com o espirito.'
-    document.body.style.color = "#FFFFFF"
+    alert('O itadore permite que o sukuna saia e acabe com o espirito.')
 }
-function sai4(){
-    s1.innerHTML = ""
-}
-
 
 function entrar5(){
-    s2.innerHTML = 'Somos apresentado a um antigo amigo do Satorou, o Getou.'
-    document.body.style.color = "#FFFFFF"
-}
-function sai5(){
-    s2.innerHTML = ""
+alert('Somos apresentado a um antigo amigo do Satorou, o Getou.')
 }
 
+
 function entrar6(){
-    s2.innerHTML = 'Um episódio mais focado na Nobara e no seu passado'
-    document.body.style.color = "#FFFFFF"
+   alert('Um episódio mais focado na Nobara e no seu passado')
 }
-function sai6(){
-    s2.innerHTML = ""
-}
+
+
 
 function entrar7(){
    
-    s3.innerHTML = 'Nesse episódio o Gojou conhEce um espirito chamado "JOGO", entao o satoru mostra seu RED'
-    document.body.style.color = "#FFFFFF"
+    alert('Nesse episódio o Gojou conhEce um espirito chamado "JOGO", entao o satoru mostra seu RED')
+
+ 
 }
 
-function sai7(){
-    s3.innerHTML = ""
-}
+
+
 
 function entrar8(){
-    s3.innerHTML = 'Somos apresentados ou estudantes de outras escolas jujutsu, em especial Aoi Todou'
-    document.body.style.color = "#FFFFFF"
+  alert('Somos apresentados ou estudantes de outras escolas jujutsu, em especial Aoi Todou')
 }
-function sai8(){
-    s3.innerHTML = ""
-}
+
 
 function entrar9(){
-    s4.innerHTML = 'Somos apresentado ao Nanami, um grande amigo do gojou, e professor do Itadori por um tempo'
-    document.body.style.color = "#FFFFFF"
+alert('Somos apresentado ao Nanami, um grande amigo do gojou, e professor do Itadori por um tempo')
+
 }
-function sai9(){
-    s4.innerHTML = ""
-}
+
 
 function entrar10(){
-    s4.innerHTML = 'O espirito Mahito tem uma aparição mais importante, e vira um grande amigo de Jumpei'
-    document.body.style.color = "#FFFFFF"
+    alert('O espirito Mahito tem uma aparição mais importante, e vira um grande amigo de Jumpei')
 }
-function sai10(){
-    s4.innerHTML = ""
-}
+
+
 
 function entrar11(){
-    s4.innerHTML = 'O espirito Mahito mata o Jumpei com a transfiguração inerte'
-    document.body.style.color = "#FFFFFF"
-}
-function sai11(){
-    s4.innerHTML = ""
+    alert('O espirito Mahito mata o Jumpei com a transfiguração inerte')
+
 }
 
+
 function entrar12(){
-    s4.innerHTML = 'Itadori pede pro Sukuna tomar conta e amtar o Mahito, mas o sukuna recusa e deix ao Itadori de xereca   '
-    document.body.style.color = "#FFFFFF"
-}
-function sai12(){
-    s4.innerHTML = ""
+    alert('Itadori pede pro Sukuna tomar conta e amtar o Mahito, mas o sukuna recusa e deix ao Itadori de xereca   ')
 }
